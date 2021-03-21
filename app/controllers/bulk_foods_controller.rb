04f596275e3f8682a9f9fed3fbc11a1316b1d516
zainonrails <zain@sendoso.com>
2021-03-21 21:32:19 +0500

    removed byebug

--- app/controllers/bulk_foods_controller.rb
+++ app/controllers/bulk_foods_controller.rb
@@ -2,7 +2,6 @@ class BulkFoodsController < ApplicationController
   def new; end
 
   def create
-    byebug
     res = ::Foods::Importer.call(params[:file].path, current_user.id)
     respond_to do |format|
       if res.errors.present?
